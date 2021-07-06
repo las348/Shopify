e1b3edb69f0dd0af7e6eac5a3e9f64af87d817a0
Laureni Wilkinson <las348@nyu.edu>
2021-07-06 15:27:19 -0400

    sorting

--- laws19/assets/application.js
+++ laws19/assets/application.js
@@ -1,1 +1,7 @@
 // Put your application javascript here
+document.querySelector('#sort_by').addEventListener('change', function(e) {
+    var url = new URL(window.location.href);
+    url.searchParams.set('sort_by', e.currentTarget.value);
+
+    window.location = url.href;
+});
